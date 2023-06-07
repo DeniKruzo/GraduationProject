84103a9f937b007b9b298fb38849ce4a2c22592a
kruzo <kruzodanil@mail.ru>
2023-06-07 16:28:06 +0500

    toggleForm

---
+++ GraduationProject/Components/MenuSpecializationViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GraduationProject.Components
+{
+    public class MenuSpecializationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            
+
+            return View();
+        }
+    }
+}
